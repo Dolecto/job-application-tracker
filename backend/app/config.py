ee69fc0c9e01348a3b3e29baaac77f88c4ab1676
Dolecto <115491148+Dolecto@users.noreply.github.com>
2026-08-20 01:07:23 +0200

    chore: set up db config (dev)

---
+++ backend/app/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # database connection // 
+    # db user?
+    # db password?
+
+    # Tavily
+    # n8n?
+    # Gmail thing?
+    # LangChain
+
+    # mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
+    model_config = SettingsConfigDict(
+        env_file='.env-dev', 
+        env_file_encoding='utf-8'
+    )
+
+    # NOTE: loaded in order
+    database_url: str
+
+settings = Settings()
